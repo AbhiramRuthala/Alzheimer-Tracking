474c0b688bd0a320236de03dd559960c7719f729
Abhiram Ruthala <134224393+AbhiramRuthala@users.noreply.github.com>
2024-01-26 10:24:50 -0500

    Update AlzheimerCalc.py

--- AlzheimerCalc.py
+++ AlzheimerCalc.py
@@ -1,14 +1,24 @@
-#Input to check for a certain neurotransmitter
+import time
+
+#Input to take the neurotransmitter
 HormoneTracker = input("Which hormone are you tracking? ")
 
-#Dictionary for neurotransmitter levels
+#Dictionary for intial neurotransmitter levels
 neuronLevels = {
     "acetylcholine": 90,
     "dopamine": 85,
-    "Gaba":75,
+    "GABA":75,
     "serotonin":70
 }
 
-# Function will check for neurotransmitter levels and the effect posed by Alzheimer's.
+# Function will check for neuron levels and the effect posed by Alzheimer's.
 def AlzheimerSim():
     AlzheimerDiseaseCalc = neuronLevels.get(HormoneTracker, " ")
+    result = AlzheimerDiseaseCalc - 14
+    print("The Alzheimer Simulation is being conducted...")
+    time.sleep(1.5)
+    print("Original value:" + str(neuronLevels.get(HormoneTracker)))
+    print("Final value:" + str(result))
+
+# Alzheimer's Disease will be simulated on sample neurotransmitter values.
+AlzheimerSim()
