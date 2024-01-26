3c1b2b908358db942a43c2deae027070d538ccfe
Abhiram Ruthala <134224393+AbhiramRuthala@users.noreply.github.com>
2024-01-25 22:50:03 -0500

    Create neuronIde.py

---
+++ neuronIde.py
@@ -0,0 +1,22 @@
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+import AlzheimerCalc
+
+HormoneTracker = input("Which hormone are you looking for? ")
+
+try
+
+neuron = [
+    ["acetylcholine"],
+    ["dopamine"],
+    ["serotonin"],
+    ["GABA"],
+    ["Endorphins"],
+    ["glutamate"],
+]
+
+
+
+
+AlzheimerDiseaseCalc = neuronLevels.get(HormoneTracker)
+
