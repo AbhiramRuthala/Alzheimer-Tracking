11f42aa80915683d88e1da50578eefbad5fd7caf
Abhiram Ruthala <134224393+AbhiramRuthala@users.noreply.github.com>
2024-01-25 22:57:22 -0500

    Update proteinTracking.py

--- proteinTracking.py
+++ proteinTracking.py
@@ -14,12 +14,12 @@ initialProteinValue = -----
 
 userRate = 2
 
-
+#Check for both and see how much they build up by.
 betaAmyloid = 15+userRate
 tauTangles = 17+userRate
 
 
-
+#Classify different protein aggregates that help cause neuron degeneration during Alzheimer's and give them specific functions.
 class ProteinAggregates:
     def trackBetaAmyloid(self, betaAmyloid):
         self.BetaAmyloid = betaAmyloid
