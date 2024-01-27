4368cb3a29202a1c1d04662c2a24910cb5c02537
Abhiram Ruthala <134224393+AbhiramRuthala@users.noreply.github.com>
2024-01-26 22:48:24 -0500

    Update proteinTracking.py

--- proteinTracking.py
+++ proteinTracking.py
@@ -1,3 +1,42 @@
+
+# --------- PLEASE READ -------------
+# This still requires some major work 
+
+# Input current protein values
+
+proteinInput = input("Protein: ")
+
+
+if proteinInput == "beta amyloid":
+    betaAmyloidInput = float(input("Amount of beta-amyloid present: "))
+elif proteinInput == "tau tangles":
+    tauTanglesInput = float(input("Amount of tau-tangles present: "))
+else:
+    print('Invalid protein name!')
+
+
+# Dictionary to store inputted protein values
+proteinTracking={
+    "beta-amyloid plaques":betaAmyloidInput,
+    "tau tangles":tauTanglesInput
+}
+
+def ProteinSense():
+    proteinThreshold = 47.265
+    if betaAmyloidInput > proteinThreshold or tauTanglesInput > proteinThreshold:
+        print("Dangerous build-up of the protein")
+    elif betaAmyloidInput < proteinThreshold and tauTanglesInput > proteinThreshold:
+        print("Dangerous build-up of the tau tangles protein complex")
+    elif betaAmyloidInput > proteinThreshold and tauTanglesInput < proteinThreshold:
+        print("Dangerous build-up of the beta amyloid protein complex")
+    else:
+        print("The protein build-up is safe for now")
+
+
+
+
+
+
 # maybe use this list
 proteinTracking=[
     [beta-amyloid plaques]
